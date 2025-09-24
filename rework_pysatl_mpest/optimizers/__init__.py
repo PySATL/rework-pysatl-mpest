1b3ca91e8a74125f6b8ed6f992b5f2155d500794
iraedeus <dtotjmyanin@mail.ru>
2025-09-24 14:26:26 +0300

    docs: optimizers

--- rework_pysatl_mpest/optimizers/__init__.py
+++ rework_pysatl_mpest/optimizers/__init__.py
@@ -6,5 +6,7 @@ __license__ = "SPDX-License-Identifier: MIT"
 
 
 from rework_pysatl_mpest.optimizers.optimizer import Optimizer
+from rework_pysatl_mpest.optimizers.scipy_nelder_mead import ScipyNelderMead
+from rework_pysatl_mpest.optimizers.scipy_powell import ScipyPowell
 
-__all__ = ["Optimizer"]
+__all__ = ["Optimizer", "ScipyNelderMead", "ScipyPowell"]
--- rework_pysatl_mpest/optimizers/scipy_nelder_mead.py
+++ rework_pysatl_mpest/optimizers/scipy_nelder_mead.py
@@ -1,3 +1,10 @@
+"""A module that provides a Nelder-Mead optimizer using the SciPy library."""
+
+__author__ = "Danil Totmyanin"
+__copyright__ = "Copyright (c) 2025 PySATL project"
+__license__ = "SPDX-License-Identifier: MIT"
+
+
 from typing import Callable
 
 from scipy.optimize import minimize
@@ -6,5 +13,43 @@ from rework_pysatl_mpest.optimizers.optimizer import Optimizer
 
 
 class ScipyNelderMead(Optimizer):
+    """An optimizer that uses the Nelder-Mead simplex algorithm from SciPy.
+
+    This class serves as a wrapper for the `scipy.optimize.minimize` function,
+    specifically configured to use the 'Nelder-Mead' method. The Nelder-Mead
+    algorithm is a direct search method that does not require gradient
+    information, making it suitable for non-differentiable or noisy objective
+    functions.
+
+    Methods
+    -------
+    .. autosummary::
+        :toctree: generated/
+
+        minimize
+    """
+
     def minimize(self, target: Callable, params: list[float]) -> list[float]:
+        """Minimizes a target function using the Nelder-Mead algorithm.
+
+        This method leverages the `scipy.optimize.minimize` function to find
+        the parameters that minimize the provided objective function.
+
+        Parameters
+        ----------
+        target : Callable
+            The objective function to minimize. It must be a callable that
+            accepts a list or NumPy array of parameters and returns a single
+            scalar value.
+        params : list[float]
+            A list of initial values for the parameters that serves as the
+            starting point for the optimization.
+
+        Returns
+        -------
+        list[float]
+            A list containing the set of parameters that minimizes the target
+            function, as found by the Nelder-Mead algorithm.
+        """
+
         return list(minimize(target, params, method="Nelder-Mead").x)
