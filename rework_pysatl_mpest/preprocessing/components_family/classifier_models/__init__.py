4ca7a55f12cadb17f4171af1f6c41947e1a00d07
Mmorgenn <marknsya951@gmail.com>
2025-11-01 16:31:54 +0300

    fix: classifier interface

--- rework_pysatl_mpest/preprocessing/components_family/classifier_models/__init__.py
+++ rework_pysatl_mpest/preprocessing/components_family/classifier_models/__init__.py
@@ -5,10 +5,10 @@ __copyright__ = "Copyright (c) 2025 PySATL project"
 __license__ = "SPDX-License-Identifier: MIT"
 
 from rework_pysatl_mpest.preprocessing.components_family.classifier_models.classifier_interface import (
-    ClassifierInterface,
+    IClassifiere,
 )
 from rework_pysatl_mpest.preprocessing.components_family.classifier_models.classifier_models import (
     XGBClassifier,
 )
 
-__all__ = ["ClassifierInterface", "XGBClassifier"]
+__all__ = ["IClassifiere", "XGBClassifier"]
--- rework_pysatl_mpest/preprocessing/components_family/classifier_models/classifier_models.py
+++ rework_pysatl_mpest/preprocessing/components_family/classifier_models/classifier_models.py
@@ -7,19 +7,24 @@ __license__ = "SPDX-License-Identifier: MIT"
 import numpy as np
 import xgboost as xgb
 from rework_pysatl_mpest.preprocessing.components_family.classifier_models.classifier_interface import (
-    ClassifierInterface,
+    IClassifiere,
 )
 
 
-class XGBClassifier(ClassifierInterface):
+class XGBClassifier(IClassifiere):
     """Implementation of XGBoosting-based classifier"""
 
     def __init__(self) -> None:
-        super().__init__()
         self.model = xgb.Booster()
+        self._is_fitted: bool = False
 
-    def _load_model(self, model_path: str) -> None:
+    @property
+    def is_fitted(self) -> bool:
+        return self._is_fitted
+
+    def load_model(self, model_path: str) -> None:
         self.model.load_model(model_path)
+        self._is_fitted = True
 
     def predict(self, criterions: dict[str, float]) -> np.ndarray:
         feature_names = list(criterions.keys())
