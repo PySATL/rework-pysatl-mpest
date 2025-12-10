65e696966e8461cde9fc655e7945fa1bda5b8612
iraedeus <dtotjmyanin@mail.ru>
2025-12-11 00:38:01 +0300

    fix: import in __init__ in _strategies module

--- rework_pysatl_mpest/estimators/iterative/_strategies/__init__.py
+++ rework_pysatl_mpest/estimators/iterative/_strategies/__init__.py
@@ -7,14 +7,13 @@ __license__ = "SPDX-License-Identifier: MIT"
 
 from collections.abc import Callable
 
-from observed_data_likelihood import observed_data_likelihood_strategy as _observed_data_likelihood_strategy
-
 from ....distributions import ContinuousDistribution
 from ....optimizers import Optimizer
 from ....typings import DType
 from ..pipeline_state import PipelineState
 from ..steps import OptimizationBlock
 from .moments import moments_strategy as _moments_strategy
+from .observed_data_likelihood import observed_data_likelihood_strategy as _observed_data_likelihood_strategy
 from .q_function import q_function_strategy as _q_function_strategy
 
 q_function_strategy: Callable[
