a2b9d1950e64f54e804fe76626996a83c5cbf900
iraedeus <dtotjmyanin@mail.ru>
2026-07-02 18:16:33 +0300

    fix: mixture runtime errors

--- pysatl_mpest/core/mixture.py
+++ pysatl_mpest/core/mixture.py
@@ -117,7 +117,7 @@ class MixtureModel:
         if np.any(weights < 0):
             raise ValueError("Weights must be positive.")
 
-        if not np.isclose(np.sum(weights), self.np.float64(1.0)):
+        if not np.isclose(np.sum(weights), np.float64(1.0)):
             raise ValueError(f"Sum of the weights must be equal 1, but it equal {np.sum(weights)}.")
 
     @property
@@ -197,12 +197,12 @@ class MixtureModel:
         if not (0 < weight < 1):
             raise ValueError("The weight of the new component must be in the range (0, 1).")
 
-        d_weight = self.np.float64(weight)
-        self._log_weights += np.log(self.np.float64(1.0) - d_weight)
+        d_weight = np.float64(weight)
+        self._log_weights += np.log(np.float64(1.0) - d_weight)
         new_log_weight = np.log(d_weight)
         self._log_weights = np.append(self._log_weights, new_log_weight)
 
-        new_component = component.astype(np.float64)
+        new_component = copy(component)
         self._components.append(new_component)
         self._cached_weights = None
         self._sorted_pairs_cache = None
